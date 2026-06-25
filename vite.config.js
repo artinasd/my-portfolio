1b69c28e8d7bd8e5b024c47c2fc804868e24c1be
Artin <am8.artin@gmail.com>
2026-06-25 21:38:08 +0330

    done

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  // Change this back to your repository name exactly
-  base: './',
+  // Changed from '/my-portfolio/' to '/' for custom domain / standard root deployment
+  base: '/my-portfolio/',
 })
